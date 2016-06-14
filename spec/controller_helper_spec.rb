e91af2923f82a1a50aa1e6034a78cc761eee5d03
Guillaume FORTAINE <guillaume@slaask.com>
2016-06-14 11:49:51 +0200

    prerender: false by default

--- spec/controller_helper_spec.rb
+++ spec/controller_helper_spec.rb
@@ -14,7 +14,7 @@ RSpec.describe TestController, type: :controller do
     end
 
     it 'renders the application layout' do
-      get :index, no_prerender: true
+      get :index, no_prerender: false
       expect(response).to render_template(layout: :application)
     end
   end
